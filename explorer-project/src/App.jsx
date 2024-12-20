acd86433cfd80a6b9e5232b2dc7c75b9b8f03492
bhavesh-200 <bhaveshvaryani2@gmail.com>
2024-12-20 10:52:55 +0530

    git

---
+++ explorer-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Users from './components/Users'
+function App() {
+  return (
+    <>
+      <h1>User Information App</h1>
+
+      <Users name="BHAVESH" age="21" email="bhaveshvaryani2@gmail.com"         />
+    </>
+  )
+}
+
+export default App
---
+++ explorer-project/src/components/Users.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+export default function Users({name , age , email}) {
+  return (
+    <>
+      <h1>name : {name}</h1>
+      <h1>age : {age}</h1>
+      <h1>email : {email}</h1>
+    </>
+  )
+}
